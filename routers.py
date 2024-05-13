ccdfd586414f51294f18b25bb38797bc22441e6b
Michael Polonskij <michael.polonskij@gmx.de>
2024-05-14 00:07:30 +0200

    feat: First try running Ollama w/ struct. output

--- routers.py
+++ routers.py
@@ -11,16 +11,18 @@ from typing import Literal
 from dotenv import find_dotenv, load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_core.runnables import (
     RunnableLambda,
     RunnableBranch,
     RunnablePassthrough,
 )
-from langchain_experimental.llms.ollama_functions import OllamaFunctions
+from langchain_openai import ChatOpenAI
 from loguru import logger
 from pydantic import BaseModel, Field
 from pyprojroot import here
+
+from ollama_functions import OllamaFunctions
 
 
 class ChainNavigator(BaseModel):
@@ -51,24 +53,37 @@ class StormCauseInspector(BaseModel):
 if __name__ == "__main__":
     _ = load_dotenv(find_dotenv())
     api_key = os.getenv("OPENAI_API_KEY")
-    llm = OllamaFunctions(model="llama3", temperature=0, )
 
-    navigator_llm = llm.with_structured_output(ChainNavigator)
+    llm = OllamaFunctions(model="llama3", temperature=0, format="json")
+
+    type_llm = llm.with_structured_output(ChainNavigator)
 
     SYSTEM = """
-        You are a dedicated assistant for insurance claims handling. 
-        You help process relevant information and classify claims to the 
-        relevant type. However, you do not have access to current events. 
+        You are a dedicated assistant for insurance claims handling.
+        You help process relevant information and classify claims to the
+        relevant type. However, you do not have access to current events.
+        Process the document and classify the claim type. 
+        Document: {document}
     """
 
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", SYSTEM),
-            ("human", "{document}"),
-        ]
+    prompt = PromptTemplate.from_template(
+        """
+        You are a dedicated assistant for insurance claims handling.
+        You help process relevant information and classify claims to the
+        relevant type. However, you do not have access to current events.
+        Process the document and classify the claim type. 
+        Document: {document}
+        """
     )
 
-    navigator = prompt | navigator_llm
+    # prompt = ChatPromptTemplate.from_messages(
+    #     [
+    #         ("system", SYSTEM),
+    #         ("human", "{document}"),
+    #     ]
+    # )
+
+    navigator = prompt | type_llm
 
     data = PyPDFLoader(str(here("./schadenmeldung.pdf"))).load()
     logger.info(data)
@@ -77,67 +92,67 @@ if __name__ == "__main__":
     logger.info("Simple Chain successfully invoked.")
     logger.info(response.get("claim_type"))
 
-    type_chain = (
-            prompt
-            | navigator_llm
-            | RunnableLambda(lambda x: x["claim_type"])
-            | StrOutputParser()
-    )
-
-    logger.info(type_chain.invoke({"document": data[0].page_content}))
-    logger.info("Chain with RunnableLambda successfully invoked.")
-
-    storm_inspector_llm = llm.with_structured_output(StormCauseInspector)
-
-    storm_cause_chain = (
-            prompt
-            | storm_inspector_llm
-            | RunnableLambda(lambda x: x["cause"])
-            | StrOutputParser()
-    )
-
-    branch = RunnableBranch(
-        (
-            lambda x: "storm" in x["claim_type"],
-            storm_cause_chain,
-        ),
-        RunnableLambda(lambda _: "default-value"),
-    )
-
-    complete_chain = {
-                         "claim_type": type_chain,
-                         "document": lambda x: x["document"],
-                     } | branch
-
-    final_respone = complete_chain.invoke({"document": data[0].page_content})
-    logger.info(final_respone)
-
-    logger.info("Rounting with functional approach")
-
-
-    def route(info):
-        if "storm" in info["document"]:
-            return storm_cause_chain
-        else:
-            return "default-value"
-
-
-    sequential_chain = (
-            {"document": type_chain} | RunnableLambda(route) | StrOutputParser()
-    )
-    logger.info(sequential_chain.invoke({"document": data[0].page_content}))
-
-
-    def routing(info):
-        if "storm" in info["document"]:
-            return RunnablePassthrough.assign(cause=storm_cause_chain)
-        else:
-            return "default-value"
-
-
-    logger.info("Functional Routing with intermediate results")
-    seq_chain = {"document": type_chain} | RunnableLambda(routing)
-
-    structured_response = seq_chain.invoke({"document": data[0].page_content})
-    logger.info(type(structured_response))
-    logger.info(structured_response)
+    # type_chain = (
+    #         prompt
+    #         | type_llm
+    #         | RunnableLambda(lambda x: x["claim_type"])
+    #         | StrOutputParser()
+    # )
+    #
+    # logger.info(type_chain.invoke({"document": data[0].page_content}))
+    # logger.info("Chain with RunnableLambda successfully invoked.")
+    #
+    # storm_inspector_llm = llm.with_structured_output(StormCauseInspector)
+    #
+    # storm_cause_chain = (
+    #         prompt
+    #         | storm_inspector_llm
+    #         | RunnableLambda(lambda x: x["cause"])
+    #         | StrOutputParser()
+    # )
+    #
+    # branch = RunnableBranch(
+    #     (
+    #         lambda x: "storm" in x["claim_type"],
+    #         storm_cause_chain,
+    #     ),
+    #     RunnableLambda(lambda _: "default-value"),
+    # )
+    #
+    # complete_chain = {
+    #                      "claim_type": type_chain,
+    #                      "document": lambda x: x["document"],
+    #                  } | branch
+    #
+    # final_respone = complete_chain.invoke({"document": data[0].page_content})
+    # logger.info(final_respone)
+    #
+    # logger.info("Rounting with functional approach")
+    #
+    #
+    # def route(info):
+    #     if "storm" in info["document"]:
+    #         return storm_cause_chain
+    #     else:
+    #         return "default-value"
+    #
+    #
+    # sequential_chain = (
+    #         {"document": type_chain} | RunnableLambda(route) | StrOutputParser()
+    # )
+    # logger.info(sequential_chain.invoke({"document": data[0].page_content}))
+    #
+    #
+    # def routing(info):
+    #     if "storm" in info["document"]:
+    #         return RunnablePassthrough.assign(cause=storm_cause_chain)
+    #     else:
+    #         return "default-value"
+    #
+    #
+    # logger.info("Functional Routing with intermediate results")
+    # seq_chain = {"document": type_chain} | RunnableLambda(routing)
+    #
+    # structured_response = seq_chain.invoke({"document": data[0].page_content})
+    # logger.info(type(structured_response))
+    # logger.info(structured_response)
