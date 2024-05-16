dc48cc23569a8b36cc21753f9400128a939750e8
Michael Polonskij <michael.polonskij@gmx.de>
2024-05-16 23:25:18 +0200

    feat: First attempt f/ routing w/ Ollama
    Not working yet

--- routing_ollama.py
+++ routing_ollama.py
@@ -7,8 +7,10 @@ This module demonstrates the routing concept in LangChain using Ollama
 from typing import Literal
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from loguru import logger
 from pyprojroot import here
 
@@ -40,10 +42,17 @@ class StormCauseInspector(BaseModel):
     )
 
 
+def routing(info):
+    if "storm" in info["document"]:
+        return RunnablePassthrough.assign(cause=storm_cause_chain)
+    else:
+        return "default-value"
+
+
 if __name__ == "__main__":
     llm = OllamaFunctions(model="llama3", temperature=0, format="json")
 
-    ollama_model = llm.with_structured_output(ChainNavigator)
+    ollama_structured_model = llm.with_structured_output(ChainNavigator)
 
     SYSTEM = """
     You are a dedicated assistant for insurance claims handling.
@@ -59,23 +68,42 @@ if __name__ == "__main__":
         ]
     )
 
-    prompt = PromptTemplate.from_template(
-        """
-        You are a dedicated assistant for insurance claims handling.
-        You help process relevant information and classify claims to the
-        relevant type. However, you do not have access to current events.
-        Process the document and classify the claim type. 
-        Document: {document}
-        """
-    )
-
-    ollama_chain = prompt | ollama_model
+    logger.info("Setting up a simple navigator chain to test Ollama")
+    navigator_chain = chat_prompt | ollama_structured_model
 
     data = PyPDFLoader(str(here("./schadenmeldung.pdf"))).load()
     logger.info(data)
 
     try:
-        response = ollama_chain.invoke({"document": data[0].page_content})
+        response = navigator_chain.invoke({"document": data[0].page_content})
         logger.info(response)
+        logger.success("Simple navigator chain test successful")
+
+        type_chain = (
+                chat_prompt
+                | ollama_structured_model
+                | RunnableLambda(lambda x: x["claim_type"])
+                | StrOutputParser()
+        )
+
+        storm_inspector_llm = llm.with_structured_output(StormCauseInspector)
+        logger.info("Storm inspector Model setup successfully")
+
+        storm_cause_chain = (
+                chat_prompt
+                | storm_inspector_llm
+                | RunnableLambda(lambda x: x["cause"])
+                | StrOutputParser()
+        )
+        logger.info("Storm cause chain setup")
+
+        seq_chain = {"document": type_chain} | RunnableLambda(routing)
+        logger.info("Sequential chain setup")
+
+        structured_response = seq_chain.invoke({"document": data[0].page_content})
+        logger.info(structured_response)
+
+        logger.success("Routing successfully completed")
+
     except Exception as e:
         logger.error(e)
